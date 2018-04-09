e26f0c3a7ac9fed869ad2d7f32c2d1e52a6a5bc2
yang <418405228@qq.com>
2018-04-10 00:22:01 +0800

    Revert "Revert "chores(1:清理项目中无用的文件)""
    This reverts commit 1ef2b93745e1ff33ede322abf603f4228ae86f52.

--- app.py
+++ app.py
@@ -13,40 +13,53 @@ from urls import url_patterns
 
 from apscheduler.schedulers.tornado import TornadoScheduler
 
+#导入GPIO设置针脚编码格式
 try:
     import RPi.GPIO as GPIO
-    print("import RPi.GPIO success 成功")
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
+
+#导入GPIO失败
 except RuntimeError:
     print("import RPi.GPIO error")
 
+#创建Tornado任务调度器
 SCHEDULER=TornadoScheduler()
 
-class TornadoDashBoard(tornado.web.Application):
+class TornadoLanternApp(tornado.web.Application):
     def __init__(self):
         tornado.web.Application.__init__(self, url_patterns, **settings)
 
+'''
+接收到进程中止信号
+1:清除针脚状态
+2:清除任务调度器中的所有任务
+'''
 def sig_handler(sig,frame):
     logging.info("GPIO cleanup and Scheduler remove all jobs")
     
     GPIO.cleanup()
     
+    SCHEDULER.remove_all_jobs()
     SCHEDULER.print_jobs()
+
+    '''
     for job in SCHEDULER.get_jobs():
         print(u'%s' % job)
-        print(u'%s' % job.args)
-    SCHEDULER.remove_all_jobs()
-    SCHEDULER.shutdown(False)
+        print(u'%s' % job.args)SCHEDULER.shutdown(False)
+    '''
 
 def main():
-    app = TornadoDashBoard()
+    #扩展app属性
+    app = TornadoLanternApp()
     app.scheduler = SCHEDULER
-    
-    http_server = tornado.httpserver.HTTPServer(app)
-    http_server.listen(options.port)
+
     app.GPIO=GPIO
     app.scheduler.start()
+
+
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
     
     signal.signal(signal.SIGTERM, sig_handler)
     signal.signal(signal.SIGINT, sig_handler)
